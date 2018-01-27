23a2cde3961624eb07f5031cf72851e68b782480
TheSamLloyd <samlloydj@gmail.com>
2018-01-27 11:17:34 -0500

    added data structure and app.js initialization

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAABGC9edTiKN4lGIA8auqRt6cg-y2LY2A",
+    authDomain: "employee-database-bcs.firebaseapp.com",
+    databaseURL: "https://employee-database-bcs.firebaseio.com",
+    projectId: "employee-database-bcs",
+    storageBucket: "employee-database-bcs.appspot.com",
+    messagingSenderId: "363408932150"
+  };
+  firebase.initializeApp(config);
